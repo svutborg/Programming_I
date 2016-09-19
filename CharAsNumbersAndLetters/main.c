6162e3432a75a628a79546c807195d5c037fbb62
Steffen Vutborg <SVU@ucn.dk>
2016-09-19 13:19:22 +0200

    Module 2
    Added examples from Module 2

---
+++ CharAsNumbersAndLetters/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    char C;
+
+    C = 'G';
+    printf("%c\n",'k'-'\n');
+    printf("%c",C+32);
+
+/*    for(i=0;i<256;i++)
+    {
+        printf("The letter %c has the nummerical value %d\n", i, i);
+    }
+*/
+
+
+    return 0;
+}
---
+++ Hello Class/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/*  This program does one thing, which is write
+    Hello All! to the console*/
+int main() // This is the main function of the program
+{
+    printf("Hello All!"); // Prints out Hello All!
+
+    return 0; // Returns 0 if the program terminated correctly
+}
