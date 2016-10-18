d144ea6f78f50884cefc23138a56684b4ff6de96
Steffen Vutborg <SVU@ucn.dk>
2016-10-18 10:28:48 +0200

    added listing 19.2

--- Listing19.2/main.c
+++ Listing19.2/main.c
@@ -1,30 +1,33 @@
 /* 19L02.c Initializing a structure */
 #include <stdio.h>
+struct employee {
+    int id;
+    char name[32];
+};
+
+void printEmployeeInfo(struct employee e);
 
 int main(void)
 {
-    struct employee {
-        int id;
-        char name[32];
-    };
-    /* structure initialization */
-    struct employee info = {
-        1,
-        "B. Smith"
-    };
 
-    printf("Here is a sample:\n");
-    printf("Employee Name: %s\n", info.name);
-    printf("Employee ID #: %04d\n\n", info.id);
+    /* structure initialization */
+    struct employee info = {1,"B. Smith"};
+
+    printEmployeeInfo(info);
 
     printf("What's your name?\n");
     gets(info.name);
     printf("What's your ID number?\n");
     scanf("%d", &info.id);
 
-    printf("\nHere are what you entered:\n");
-    printf("Name: %s\n", info.name);
-    printf("ID #: %04d\n", info.id);
+    printEmployeeInfo(info);
 
     return 0;
+}
+
+void printEmployeeInfo(struct employee e)
+{
+    printf("Employee info:\n");
+    printf("Employee Name: %s\n", e.name);
+    printf("Employee ID #: %04d\n\n", e.id);
 }
